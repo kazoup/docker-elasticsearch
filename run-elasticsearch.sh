3d674d7e7f9449386882ee1fa1d90596409df67d
Nicolas Trésegnie <nicolas.tresegnie@kazoup.com>
2014-06-24 09:06:26 +0100

    Increase the maximum number of open files

--- run-elasticsearch.sh
+++ run-elasticsearch.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -ex
 
+# Increase the maximum number of open files
+# The container need to be run in privileged mode or this will throw an error
+ulimit -n 65535
+
 exec /srv/elasticsearch/bin/elasticsearch -f 2>&1
